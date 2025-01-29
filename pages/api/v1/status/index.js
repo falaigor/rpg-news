e74494066f8c1d3303cd53f8d0061b2905abb1de
Igor Santos <falaigors@gmail.com>
2025-01-29 15:07:38 +0000

    finish '/status' endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,16 +1,24 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const updatedAt = new Date().toISOString()
+  const updatedAt = new Date().toISOString();
 
-  const databaseVersionResult = await database.query("SHOW server_version;")
-  const databaseVersionValue = databaseVersionResult.rows[0].server_version
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  const databaseMaxConnectionsResult = await database.query("SHOW max_connections;")
-  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
 
-  const databaseOpenedConnectionsResult = await database.query("SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';")
-  const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
@@ -18,9 +26,9 @@ async function status(request, response) {
       database: {
         version: databaseVersionValue,
         max_connections: parseInt(databaseMaxConnectionsValue),
-        opened_connections: databaseOpenedConnectionsValue
-      }
-    }
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
   });
 }
 
