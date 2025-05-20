c31b4aea9ad4c8560cd6cc17db193c2a8c81724b
falaigors <falaigors@gmail.com>
2025-05-20 14:36:04 +0000

    update validation 'orchestrator.js' with validation fetch response

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -2,6 +2,7 @@ import retry from "async-retry"
 
 const MAX_TIMEOUT_IN_MILLISECONDS = 1_000
 const RETRIES = 100
+const HTTP_STATUS_OK = 200
 
 async function waitForAllServices() {
   async function waitForWebServer() {
@@ -12,7 +13,10 @@ async function waitForAllServices() {
 
     async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status")
-      await response.json();
+
+      if (response.status !== HTTP_STATUS_OK) {
+        throw Error();
+      }
     }
   }
 
