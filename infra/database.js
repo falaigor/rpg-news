b1fadf5fa71359c880bdaf06c4ff84e000b9d4c0
falaigors <falaigors@gmail.com>
2025-05-21 01:58:04 +0000

    fix  quality linting

--- infra/database.js
+++ infra/database.js
@@ -40,7 +40,9 @@ function getSSLValues() {
   return process.env.NODE_ENV === "production" ? true : false;
 }
 
-export default {
+const database = {
   query,
-  getNewClient,
-};
+  getNewClient
+}
+
+export default database
